995a69884d911c0d729eca41ad092f9788484a41
Tor-Elesh <torelesh775@gmail.com>
2024-06-18 10:47:37 +0100

    Update airbnbdash.py

--- airbnbdash.py
+++ airbnbdash.py
@@ -42,7 +42,7 @@ col5.metric("Snitt antall senger",averagebeds.round(2).astype(str) + ' Senger')
 
 
 #Row B
-c1,c2 = st.columns((5,5)
+c1,c2 = st.columns((5,5))
 with c1:
     st.markdown('Snitt Pris over tid')
     st.plotly_chart(px.line(filtered_data.groupby('check in')['price (NOK)'].mean().reset_index(),
